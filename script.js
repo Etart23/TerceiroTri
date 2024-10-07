defe6c04553d19ffa4569c3277e13c13ad5d1958
Etart23 <yagohuk2332@gmail.com>
2024-10-07 07:35:59 -0300

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+function showAnswer(card) {
+    const answer = card.querySelector('.card-answer');
+    if (answer.style.display === 'none') {
+        answer.style.display = 'block';
+    } else {
+        answer.style.display = 'none';
+    }
+}
