9eee8e36c43e106e6a3fcb87f9a03d464eae9edb
Etart23 <yagohuk2332@gmail.com>
2024-10-07 07:29:19 -0300

    .

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const text = "Este é o texto que aparecerá um caractere por vez.";
-    const cardAnswer = document.getElementById('card-answer');
-    let index = 0;
-
-    document.addEventListener('keydown', (event) => {
-        if (event.code === 'Space') {
-            event.preventDefault(); // Evita o comportamento padrão da barra de espaço
-            if (index < text.length) {
-                cardAnswer.textContent += text[index];
-                index++;
+function showAnswer(card) {
+const answer = card.querySelector('.card-answer');
+            if (answer.style.display === 'none') {
+                answer.style.display = 'block';
+            } else {
+                answer.style.display = 'none';
             }
         }
-    });
-});
